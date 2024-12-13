670dadcc656ffd176e8e9513b35b4ada95b9192e
Kevin <fabianuskevin147@gmail.com>
2024-12-13 23:12:37 +0700

    [submit frontEnd] configure web interface using tailwind

--- vin/main.js
+++ vin/main.js
@@ -1,9 +1,5 @@
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, 'foo');
-});
+// function alert(string) {
+//   console.log(string);
+// }
 
-Promise.all([promise1, promise2, promise3]).then((values) => {
-  console.log(values);
-});
+alert('Dicoding Indonesia');
